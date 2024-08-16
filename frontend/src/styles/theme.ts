2a45a02b32ac5bf764e7b62566b7d044701dfbbb
Kl3va <ikwunzekelvin@gmail.com>
2024-08-16 14:57:22 +0100

    feat: created themes(light n dark)

---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,38 @@
+export interface ThemeTypes {
+  bg100: string
+  bg200: string
+  bg300: string
+  bg400: string
+  bg500: string
+  bg600: string
+  font100: string
+  font200: string
+  font300: string
+  border100: string
+}
+
+export const lightTheme = {
+  bg100: '#F8F8FB',
+  bg200: '#373B53',
+  bg300: '#FFFFFF',
+  bg400: '#F9FAFE',
+  bg500: '#373B53',
+  bg600: 'DFE3FA',
+  font100: '#0C0E16',
+  font200: '#888EB0',
+  font300: '#7E88C3',
+  border100: '#DFE3FA',
+}
+
+export const darkTheme = {
+  bg100: '#141625',
+  bg200: '#373B53',
+  bg300: '#1E2139',
+  bg400: '#252945',
+  bg500: '#0C0E16',
+  bg600: '#252945',
+  font100: '#FFFFFF',
+  font200: '#DFE3FA',
+  font300: '#DFE3FA',
+  border100: '#252945',
+}
