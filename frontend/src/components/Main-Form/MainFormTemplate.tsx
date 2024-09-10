697b28fda847bb457c2f1002eb2bbb6c8e8d295f
Kl3va <ikwunzekelvin@gmail.com>
2024-09-10 15:20:39 +0100

    feat: List item container styled

--- frontend/src/components/Main-Form/MainFormTemplate.tsx
+++ frontend/src/components/Main-Form/MainFormTemplate.tsx
@@ -1,10 +1,13 @@
 import GobackButton from 'components/GobackButton/GobackButton'
 import {
+  CurrencyContainer,
   DateAndTermsContainer,
   FormButtonsContainer,
   FormInvoiceContainer,
   GobackFormBtnWrapper,
   InvoiceFormContainer,
+  ItemListWrapper,
+  ListContainer,
   LocationContainer,
 } from 'components/Main-Form/MainFormTemplateStyles'
 
@@ -99,7 +102,7 @@ const MainFormTemplate = () => {
             <input type='text' placeholder='Graphic Design' required />
           </div>
 
-          <div>
+          <CurrencyContainer>
             <label>Currency</label>
             <select name='terms' id='terms' required>
               <option value='select'>Select Currency</option>
@@ -108,30 +111,30 @@ const MainFormTemplate = () => {
               <option value='pound'>£: Pound</option>
               <option value='euro'>€: Euro</option>
             </select>
-          </div>
+          </CurrencyContainer>
         </fieldset>
 
-        <fieldset>
+        <ItemListWrapper>
           <legend>Item List</legend>
-          <div>
+          <ListContainer>
             <div>
               <label>Item Name</label>
-              <input type='text' />
+              <input type='text' placeholder='logo design' required />
             </div>
 
             <div>
-              <label htmlFor=''>Country</label>
-              <input type='text' />
+              <label htmlFor=''>Qty.</label>
+              <input type='number' required />
             </div>
 
             <div>
-              <label htmlFor=''>Country</label>
-              <input type='text' />
+              <label htmlFor=''>Price</label>
+              <input type='number' required />
             </div>
 
             <div>
-              <label htmlFor=''>Country</label>
-              <p>value</p>
+              <label htmlFor=''>Total</label>
+              <p>0</p>
             </div>
 
             <svg width='13' height='16' xmlns='http://www.w3.org/2000/svg'>
@@ -141,28 +144,29 @@ const MainFormTemplate = () => {
                 fill-rule='nonzero'
               />
             </svg>
-          </div>
+          </ListContainer>
 
-          <div>
+          <ListContainer>
             <div>
-              <label htmlFor=''>Country</label>
-              <input type='text' />
+              <label>Item Name</label>
+              <input type='text' placeholder='brand design' required />
             </div>
 
             <div>
-              <label htmlFor=''>Country</label>
-              <input type='text' />
+              <label htmlFor=''>Qty.</label>
+              <input type='number' required />
             </div>
 
             <div>
-              <label htmlFor=''>Country</label>
-              <input type='text' />
+              <label htmlFor=''>Price</label>
+              <input type='number' required />
             </div>
 
             <div>
-              <label htmlFor=''>Country</label>
-              <p>value</p>
+              <label htmlFor=''>Total</label>
+              <p>0</p>
             </div>
+
             <svg width='13' height='16' xmlns='http://www.w3.org/2000/svg'>
               <path
                 d='M11.583 3.556v10.666c0 .982-.795 1.778-1.777 1.778H2.694a1.777 1.777 0 01-1.777-1.778V3.556h10.666zM8.473 0l.888.889h3.111v1.778H.028V.889h3.11L4.029 0h4.444z'
@@ -170,8 +174,10 @@ const MainFormTemplate = () => {
                 fill-rule='nonzero'
               />
             </svg>
-          </div>
-        </fieldset>
+          </ListContainer>
+
+          <button>+ Add New Item</button>
+        </ItemListWrapper>
 
         <FormButtonsContainer>
           <button>kasds</button>
--- frontend/src/components/Main-Form/MainFormTemplateStyles.ts
+++ frontend/src/components/Main-Form/MainFormTemplateStyles.ts
@@ -54,12 +54,24 @@ export const InvoiceFormContainer = styled.form`
     -moz-appearance: none;
     background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M7 10l5 5 5-5z' fill='%237C5DFA'/%3E%3C/svg%3E");
     background-repeat: no-repeat;
-    background-position: right 0.5rem center;
+    background-position: 97.5% 55%;
     background-size: 1.5em;
 
     option {
       font-size: 1rem;
     }
+  }
+`
+
+export const ItemListWrapper = styled.fieldset`
+  button {
+    color: var(--color-font-400);
+    background-color: var(color-bg-400);
+    border-radius: 6.25rem;
+    padding-block: 1.13rem 0.94rem;
+    font-weight: 700;
+    font-size: var(--font-size-small);
+    border: none;
   }
 `
 
@@ -75,6 +87,37 @@ export const LocationContainer = styled.div`
 export const DateAndTermsContainer = styled.div`
   display: grid;
   gap: 1.56rem;
+`
+export const CurrencyContainer = styled.div`
+  width: 10rem;
+`
+export const ListContainer = styled.div`
+  display: grid;
+  grid-template-columns: 4rem 6.25rem auto fit-content(13px);
+  grid-template-rows: auto auto;
+  gap: 1rem;
+
+  & > :first-child {
+    grid-column: 1 / -1;
+    grid-row: 1 / 2;
+  }
+
+  & > :nth-child(4) {
+    display: grid;
+    grid-template-rows: min-content auto;
+
+    p {
+      align-self: center;
+      color: var(--color-font-400);
+      font-weight: 700;
+      font-size: var(--font-size-small);
+    }
+  }
+
+  & > :last-child {
+    align-self: center;
+    margin-top: 1rem;
+  }
 `
 
 export const FormButtonsContainer = styled.div`
