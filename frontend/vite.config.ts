343d87854e677a9c1663200d6cb5b043b51096cc
Kl3va <ikwunzekelvin@gmail.com>
2024-11-19 11:23:38 +0100

    refactor: removed docker configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,10 +4,4 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  server: {
-    host: '0.0.0.0', // Allow Vite to be accessible from Docker
-    watch: {
-      usePolling: true, // Enables polling to detect changes in Docker
-    },
-  },
 })
